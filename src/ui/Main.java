65908f7e37e239d83882b0221a38bdb33015e9c8
jul109 <115905294+jul109@users.noreply.github.com>
2023-03-27 19:27:29 -0500

    Prueba 1

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main{
 	public static Scanner reader=new Scanner(System.in);
 	public static void main(String args[]){
-		System.out.println("Hola");
+		System.out.println("prueba1");
 
 
 	}
