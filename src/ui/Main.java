2335de22cbbdec2e12af146f3aa9b2cc86aaa3db
jul109 <115905294+jul109@users.noreply.github.com>
2023-03-27 19:21:59 -0500

    Initial commit

---
+++ src/ui/Main.java
@@ -0,0 +1,13 @@
+package ui;
+import java.util.Scanner;
+
+public class Main{
+	public static Scanner reader=new Scanner(System.in);
+	public static void main(String args[]){
+		System.out.println("Hola");
+
+
+	}
+
+
+}
